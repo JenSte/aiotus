da5c556c61ccb7f4dbecd4d9b4608badc42750c9
Jens Steinhauser <jens.steinhauser@gmail.com>
2020-06-14 11:25:12 +0200

    Add aiohttp.Fingerprint to list of ssl argument types

--- aiotus/common.py
+++ aiotus/common.py
@@ -1,11 +1,13 @@
 import ssl
 from typing import Dict, Optional, Union
 
+import aiohttp
+
 # The version of the tus protocol we implement.
 TUS_PROTOCOL_VERSION = "1.0.0"
 
 #: The type of the 'ssl' argument passed on to aiohttp.
-SSLArgument = Optional[Union[bool, ssl.SSLContext]]
+SSLArgument = Optional[Union[bool, ssl.SSLContext, aiohttp.Fingerprint]]
 
 #: The type used to hold metadata of an upload.
 Metadata = Dict[str, Optional[bytes]]

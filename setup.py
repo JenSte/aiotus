f57b6dfcc51e27f02e3319a4167a887ed95fc160
Jens Steinhauser <jens.steinhauser@gmail.com>
2020-08-07 20:46:00 +0200

    Bump to 0.1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aiotus",
-    version="0.1.5",
+    version="0.1.6",
     author="Jens Steinhauser",
     author_email="jens.steinhauser@gmail.com",
     description="Asynchronous tus (tus.io) client library",

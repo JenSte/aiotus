c2ea604ff3f61b6389550517c066fa0a2412158b
Jens Steinhauser <jens.steinhauser@gmail.com>
2020-10-07 21:39:10 +0200

    Run CI also for python 3.9

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",

a94f3ce7b31f98509bfb38f407d494fce08201ad
Jens Steinhauser <jens.steinhauser@gmail.com>
2025-08-03 13:51:16 +0200

    Use random ports when starting the server programs

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -2,10 +2,10 @@
 
 from __future__ import annotations
 
-import io
 import os.path
-import sys
 import unittest.mock
+
+import pytest
 
 import aiotus.entrypoint
 
@@ -13,18 +13,18 @@ from . import conftest
 
 
 class TestAiotusClients:
-    def test_no_command(self) -> None:
+    def test_no_command(self, capsys: pytest.CaptureFixture[str]) -> None:
         with unittest.mock.patch("sys.argv", [""]):
-            with unittest.mock.patch("sys.stderr", new_callable=io.StringIO):
-                assert isinstance(sys.stderr, io.StringIO)
+            assert 1 == aiotus.entrypoint.main()
 
-                assert 1 == aiotus.entrypoint.main()
+        _, stderr = capsys.readouterr()
+        lines = stderr.splitlines(False)
+        assert len(lines) >= 0
+        assert lines[0] == "No command specified."
 
-                lines = sys.stderr.getvalue().splitlines(False)
-                assert len(lines) >= 0
-                assert lines[0] == "No command specified."
-
-    def test_aiotus_clients(self, tusd: conftest.TusServer) -> None:
+    def test_aiotus_clients(
+        self, capsys: pytest.CaptureFixture[str], tusd: conftest.TusServer
+    ) -> None:
         conf = aiotus.RetryConfiguration(1, 0.001, False)
         defaults = (None, None, conf, None, 4 * 1024 * 1024)
         with unittest.mock.patch.object(aiotus.upload, "__defaults__", defaults):
@@ -39,11 +39,10 @@ class TestAiotusClients:
             assert 1 == aiotus.entrypoint.main()
 
         with unittest.mock.patch("sys.argv", ["", "upload", str(tusd.url), __file__]):
-            with unittest.mock.patch("sys.stdout", new_callable=io.StringIO):
-                assert isinstance(sys.stdout, io.StringIO)
+            assert 0 == aiotus.entrypoint.main()
 
-                assert 0 == aiotus.entrypoint.main()
-                url = sys.stdout.getvalue().strip()
+        stdout, _ = capsys.readouterr()
+        url = stdout.splitlines(False)[0]
 
         expected_output = [
             f"filename: {os.path.basename(__file__)}",
@@ -51,24 +50,25 @@ class TestAiotusClients:
         ]
 
         with unittest.mock.patch("sys.argv", ["", "metadata", url]):
-            with unittest.mock.patch("sys.stdout", new_callable=io.StringIO):
-                assert 0 == aiotus.entrypoint.main()
+            assert 0 == aiotus.entrypoint.main()
 
-                lines = sys.stdout.getvalue().splitlines(False)
-                lines.sort()
+        stdout, _ = capsys.readouterr()
+        lines = stdout.splitlines(False)
+        lines.sort()
 
-                assert lines == expected_output
+        assert lines == expected_output
 
         with unittest.mock.patch("sys.argv", ["", "--debug", "metadata", url]):
-            with unittest.mock.patch("sys.stdout", new_callable=io.StringIO):
-                assert 0 == aiotus.entrypoint.main()
+            assert 0 == aiotus.entrypoint.main()
 
-                lines = sys.stdout.getvalue().splitlines(False)
-                lines.sort()
+        stdout, _ = capsys.readouterr()
+        lines = stdout.splitlines(False)
+        lines.sort()
+        assert lines == expected_output
 
-                assert lines == expected_output
-
-    def test_additional_metadata(self, tusd: conftest.TusServer) -> None:
+    def test_additional_metadata(
+        self, capsys: pytest.CaptureFixture[str], tusd: conftest.TusServer
+    ) -> None:
         with unittest.mock.patch(
             "sys.argv",
             [
@@ -82,16 +82,15 @@ class TestAiotusClients:
                 __file__,
             ],
         ):
-            with unittest.mock.patch("sys.stdout", new_callable=io.StringIO):
-                assert isinstance(sys.stdout, io.StringIO)
+            assert 0 == aiotus.entrypoint.main()
 
-                assert 0 == aiotus.entrypoint.main()
-                url = sys.stdout.getvalue().strip()
+        stdout, _ = capsys.readouterr()
+        url = stdout.splitlines(False)[0]
 
         with unittest.mock.patch("sys.argv", ["", "metadata", url]):
-            with unittest.mock.patch("sys.stdout", new_callable=io.StringIO):
-                assert 0 == aiotus.entrypoint.main()
+            assert 0 == aiotus.entrypoint.main()
 
-                lines = sys.stdout.getvalue().splitlines(False)
-                assert "key1: value1" in lines
-                assert "key2" in lines
+        stdout, _ = capsys.readouterr()
+        lines = stdout.splitlines(False)
+        assert "key1: value1" in lines
+        assert "key2" in lines
